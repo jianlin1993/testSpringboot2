f82e1fe747e8f8cf586f6260f859cf873db02c52
wang_jl <wang_jl@murongtech.com>
2020-08-18 15:51:27 +0800

    druid数据源配置

--- src/main/java/com/wxy/wjl/testspringboot2/config/DataSourceConfiguration.java
+++ src/main/java/com/wxy/wjl/testspringboot2/config/DataSourceConfiguration.java
@@ -1,10 +1,10 @@
 package com.wxy.wjl.testspringboot2.config;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -12,39 +12,104 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
+/**
+ * druid数据源配置
+ */
 @Configuration
+@Slf4j
 public class DataSourceConfiguration {
-    @Value("${spring.datasource.driver-class-name}")
-    private String jdbcDriver;
     @Value("${spring.datasource.url}")
-    private String jdbcUrl;
+    private String dbUrl;
+
     @Value("${spring.datasource.username}")
-    private String jdbcUser;
+    private String username;
+
     @Value("${spring.datasource.password}")
-    private String jdbcPassword;
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.druid.initial-size}")
+    private int initialSize;
+
+    @Value("${spring.datasource.druid.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.druid.max-active}")
+    private int maxActive;
+
+    @Value("${spring.datasource.druid.max-wait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.druid.time-between-eviction-runs-millis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.druid.min-evictable-idle-time-millis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.druid.test-while-idle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.druid.test-on-borrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.druid.test-on-return}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.druid.pool-prepared-statements}")
+    private boolean poolPreparedStatements;
+
+//    @Value("${spring.datasource.druid.max-pool-prepared-statement-per-connection-size}")
+//    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.druid.filters}")
+    private String filters;
+
+    @Value("${spring.datasource.druid.connectionProperties}")
+    private String connectionProperties;
+
+    @Value("${spring.datasource.druid.validationQuery}")
+    private String validationQuery;
 
 
-/*  原c3p0连接池
+    /**
+     * 单数据源  todo多数据源切换
+     * @return
+     */
     @Bean
-    public DataSource createDataSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    @Primary
+    public DataSource dataSource() {
+        DruidDataSource datasource = new DruidDataSource();
 
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(jdbcUser);
-        dataSource.setPassword(jdbcPassword);
-        // 关闭连接后不自动提交
-        dataSource.setAutoCommitOnClose(false);
+        datasource.setUrl(this.dbUrl);
+        datasource.setUsername(this.username);
+        datasource.setPassword(this.password);
+        datasource.setDriverClassName(this.driverClassName);
 
-        return dataSource;
-    }*/
-@Primary
-@Bean(name = "dataSource")
-@ConfigurationProperties(prefix = "spring.datasource")
-public DataSource druidDataSource() {
-    return DruidDataSourceBuilder.create().build();
-}
+        // configuration
+        datasource.setInitialSize(this.initialSize);
+        datasource.setMinIdle(this.minIdle);
+        datasource.setMaxActive(this.maxActive);
+        datasource.setMaxWait(this.maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(this.timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(this.minEvictableIdleTimeMillis);
+        datasource.setTestWhileIdle(this.testWhileIdle);
+        datasource.setTestOnBorrow(this.testOnBorrow);
+        datasource.setTestOnReturn(this.testOnReturn);
+        datasource.setPoolPreparedStatements(this.poolPreparedStatements);
+        //datasource.setMaxPoolPreparedStatementPerConnectionSize(this.maxPoolPreparedStatementPerConnectionSize);
+        datasource.setConnectionProperties(connectionProperties);
+        datasource.setValidationQuery(validationQuery);
+        try {
+            datasource.setFilters(this.filters);
+        } catch (SQLException e) {
+            log.error("druid configuration init fail!");
+        }
+
+        return datasource;
+    }
 
     /**
      * 注册一个StatViewServlet
@@ -80,7 +145,7 @@ public DataSource druidDataSource() {
         //添加过滤规则.
         filterRegistrationBean.addUrlPatterns("/*");
 
-        //添加不需要忽略的格式信息.
+        //添加忽略的格式信息.
         filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         return filterRegistrationBean;
     }
