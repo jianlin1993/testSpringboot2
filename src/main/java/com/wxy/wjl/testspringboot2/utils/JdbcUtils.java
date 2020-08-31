cf1aa5961f35034de0f4125bfdfb206ecd31a22b
wang_jl <wang_jl@murongtech.com>
2020-08-31 20:12:39 +0800

    增加jdbc处理BLOB字段

--- src/main/java/com/wxy/wjl/testspringboot2/utils/JdbcUtils.java
+++ src/main/java/com/wxy/wjl/testspringboot2/utils/JdbcUtils.java
@@ -1,39 +1,48 @@
 package com.wxy.wjl.testspringboot2.utils;
 
 import com.alibaba.druid.filter.config.ConfigTools;
+import org.apache.commons.io.input.BOMInputStream;
+import org.apache.commons.io.output.StringBuilderWriter;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static org.apache.commons.io.IOUtils.copy;
+
 /**
- * jdbc处理BLOB字段  字节流字段
+ * jdbc静态工具类
  */
-@Service
 public class JdbcUtils {
 
-    @Autowired
-    Environment env;
+    public static void main(String[] args) throws Exception{
+        String sql="SELECT BYTS FROM BSDBUI.T_WKF_GEBYTE where ID = '5465323'";
+        String result=queryBlobData(sql);
+        System.out.println(result);
+    }
 
-    public  Connection getConnection() throws Exception {
+
+    public static Connection getConnection() throws Exception {
         HashMap<String,String> dsInfMap=getConnPar();
 
         Class.forName(dsInfMap.get("driverClassName"));
         return DriverManager.getConnection(dsInfMap.get("url"), dsInfMap.get("userName"), dsInfMap.get("passWord"));
     }
 
-    private  HashMap<String,String> getConnPar() throws Exception{
+    private static HashMap<String,String> getConnPar() throws Exception{
         HashMap<String,String> dsInfMap=new HashMap<>();
-        String url = env.getProperty("ecp.datasource.ora1.url");
-        String userName = env.getProperty("ecp.datasource.ora1.username");
-        String passWord = env.getProperty("ecp.datasource.ora1.password");
-        String driverClassName = env.getProperty("ecp.datasource.ora1.driverClassName");
-        String publicKey = env.getProperty("ecp.datasource.ora1.publicKey");
+        String url = "jdbc:oracle:thin:@10.0.0.6:1521/cba";
+        String userName = "BSDBUI";
+        String passWord = "BSDBUI";
+        String driverClassName = "oracle.jdbc.driver.OracleDriver";
+        String publicKey = "";
         if(StringUtils.isNotBlank(publicKey)){
             passWord = ConfigTools.decrypt(publicKey, passWord);
         }
@@ -45,8 +54,9 @@ public class JdbcUtils {
     }
 
 
-    public  String queryBlobData(String sql) throws Exception {
+    public static String queryBlobData(String sql) throws Exception {
         String data = "";
+        String head="";
         Connection connection = getConnection();
 
         Statement statement = null;
@@ -56,7 +66,9 @@ public class JdbcUtils {
             rs = statement.executeQuery(sql);
             if(rs.next()){
                 Blob blob=rs.getBlob(1);
-                data = new String(blob.getBytes(1, (int) blob.length()),"UTF-8");
+               // data=streamToString(new BOMInputStream(blob.getBinaryStream()),StandardCharsets.UTF_8);
+                //head = new String(blob.getBytes(1, 7),StandardCharsets.UTF_16LE);
+                data = new String(blob.getBytes(8, (int) blob.length()),StandardCharsets.UTF_8);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -69,7 +81,7 @@ public class JdbcUtils {
         return data;
     }
 
-    public void close(AutoCloseable closeable) {
+    public static void close(AutoCloseable closeable) {
         if (null != closeable) {
             try {
                 closeable.close();
@@ -77,5 +89,33 @@ public class JdbcUtils {
             }
         }
 
+    }
+    public static String streamToString(InputStream input, Charset encoding) throws IOException {
+        StringBuilderWriter sw = new StringBuilderWriter();
+        Throwable var3 = null;
+
+        String var4;
+        try {
+            copy((InputStream)input, (Writer)sw, (Charset)encoding);
+            var4 = sw.toString();
+        } catch (Throwable var13) {
+            var3 = var13;
+            throw var13;
+        } finally {
+            if (sw != null) {
+                if (var3 != null) {
+                    try {
+                        sw.close();
+                    } catch (Throwable var12) {
+                        var3.addSuppressed(var12);
+                    }
+                } else {
+                    sw.close();
+                }
+            }
+
+        }
+
+        return var4;
     }
 }
